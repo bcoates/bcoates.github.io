af267becb7b861b47f8253e6c139501bef2b743e
bcoates <ben@blueskylabs.com>
2017-01-26 11:08:43 -0800

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ var ReelStrip = React.createClass({
 					ce('input', {key: 2, type: 'number', value: 42})
 				])
 			})
-			return ce('div', {key: reel_idx, style: {display: 'inline-block'}, stops)
+			return ce('div', {key: reel_idx, style: {display: 'inline-block'}}, stops)
 		})
 		return ce('div', {}, strips)
 	}
