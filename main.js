cf4f7f99351eb92f1f9e13b29234066ef873eca8
bcoates <ben@blueskylabs.com>
2017-01-26 18:58:51 -0800

    Update main.js

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ function flatreel(reelstrip, wildcards) {
 	reelstrip.forEach( e => {
 		ret[e.sym] = (ret[e.sym] || 0) + e.n
 	})
-	wildcards.entries.foreach( [pattern, symbols] => {
+	wildcards.entries.foreach( ([pattern, symbols]) => {
 		ret[pattern] = (ret[pattern] || 0) + Σ(symbols, sym => ret[sym])
 	})
 	return ret
